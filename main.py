3d5b9113421a13b51e7fe688c3ef7a4cf32f87a1
AlexK <kawr13@gmail.com>
2023-04-26 03:01:10 +0300

    Chat

--- main.py
+++ main.py
@@ -13,22 +13,27 @@ while run:
         print(help)
     elif command == 'show':
         s = input("Введите дату задачи: ")
-        for key in tasks:
-            if key == s:
-                print(key, tasks[key])
-                break
-            elif s == 'все' or s == '':
-                for key in tasks:
-                    print(key, tasks[key])
-                    break
-            else:
-                print("Задача не найдена")
-                break
+        if s in tasks.keys():
+            print(f'{s}: {tasks[s]}')
+        elif s == 'все':
+            for key, value in tasks.items():
+                print(f'{key}: {value} ')
+        else:
+            print("Задача не найдена")
     elif command == 'add':
         date = input("Введите дату задачи: ")
         task = input('введите название задачи: ')
-        tasks[date] = task
-        print('Задача добавлена')
+        task_exists = False
+        for key, value in tasks.items():
+            if key == date:
+                if task in value:
+                    if task == value:
+                        print("Задача уже существует")
+                        task_exists = True
+                        break
+        else:
+            tasks[date] = task
+            print("Задача добавлена")
     elif command == 'выход':
         run = False
         print("Досвиданье")
