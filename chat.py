578e39555d385ca9d938fa80f4a82cd8a8328872
AlexK <kawr13@gmail.com>
2023-04-26 03:39:01 +0300

    обавил функцию

--- chat.py
+++ chat.py
@@ -2,6 +2,39 @@ from datetime import datetime
 
 pass_users = {}
 
+all_messages = []
+
+chat_log = {
+    'auther': 'Александр',
+    'text': 'Привет',
+    'time': datetime.now().strftime('%H:%M')
+}
+chat_log_2 = {
+    'auther': 'Маша',
+    'text': 'Привет, Саша',
+    'time': datetime.now().strftime('%H:%M')
+}
+all_messages.append(chat_log)
+
+all_messages.append(chat_log_2)
+
+def add_message(author, text = None, password = None):
+    chat_log = {
+        'auther': author,
+        'text': text,
+        'password': password,
+        'time': datetime.now().strftime('%H:%M')
+    }
+    all_messages.append(chat_log)
+    chat_log_2 = {
+        'auther': author,
+        'text': text,
+        'password': '123',
+        'time': datetime.now().strftime('%H:%M')
+    }
+    all_messages.append(chat_log)
+
+
 while True:
     print('Добро пожаловать в чат!')
     print('Для входа введите имена и пароли')
@@ -10,6 +43,7 @@ while True:
         while True:
             name = input('Введите имя: ').lower()
             passw = input('Введите пароль: ')
+            add_message(name, passw)
             if pass_users.get(name) == passw:
                 print('Добро пожаловать')
                 break
@@ -21,21 +55,10 @@ while True:
                 print('Неверный логин или пароль')
 
         while True:
-            all_messages = []
-            chat_log = {
-                'auther': 'Александр',
-                'text': 'Привет',
-                'time': datetime.now().strftime('%H:%M')
-            }
-            chat_log = {
-                'auther': 'Маша',
-                'text': 'Привет, Саша',
-                'time': datetime.now().strftime('%H:%M')
-            }
             run = True
             while run:
                 msg = input('Введите сообщение: ')
-                chat_log[name] = msg
+                add_message(name, msg)
                 if msg == 'stop':
                     run = False
                 else:
